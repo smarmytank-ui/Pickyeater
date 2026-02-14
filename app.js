1f14c1b7e6e6be3bd448d9ce7fe573326ed9f140
smarmytank-ui <smarmytank@gmail.com>
2026-02-13 22:19:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 // SUPABASE SETUP
 // ===============================
 
-const SUPABASE_URL = https://ouxrweqfmupebjzsvnxl.supabase.co;
-const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91eHJ3ZXFmbXVwZWJqenN2bnhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwMzM4NzEsImV4cCI6MjA4NjYwOTg3MX0.nRGM2Uxx0lFN9s4--4QjSQK8UOylM7H00bP9Sduw1ek";
+const SUPABASE_URL = "https://ouxrweqfmupebjzsvnxl.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91eHJ3ZXFmbXVwZWJqenN2bnhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwMzM4NzEsImV4cCI6MjA4NjYwOTg3MX0.nRGM2Uxx0lFN9s4--4QjSQK8UOylM7H00bP9Sduw1ek";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
